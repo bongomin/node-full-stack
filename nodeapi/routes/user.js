f886371f43c020a160c9c0080d2d427705784667
bongomin <bongomindaniel@gmail.com>
2019-10-16 16:21:19 +0300

    feat(getting single user by id):
    - getting user profile by userId
    
    - the users profile atouched to UserId

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,12 +1,15 @@
 
-const { allUsers, userById } = require('../controllers/user');
+const { allUsers, userById, getUser } = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 
 // get all users
 router.get('/users', allUsers);
+//get single users
+router.get('/user/:userId', getUser);
 
 // any route containing: userId , my app will first Excecute userByid()
 router.param('userId', userById);
+
 
 module.exports = router;
