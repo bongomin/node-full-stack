9c3351c7e58509cd1cf8d4ad1b1eeea7ec26b888
bongomin <bongomindaniel@gmail.com>
2019-10-16 16:54:58 +0300

    feat(Restricting getting single information of a user):
    - user route is protected
    
    - to view a single user , one has to first signin to get a Token
    
    - after getting the Token, Use the Token To access a single user Id

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,12 +1,13 @@
 
 const { allUsers, userById, getUser } = require('../controllers/user');
+const { requireSignin } = require('../controllers/auth');
 const express = require('express');
 const router = express.Router();
 
 // get all users
 router.get('/users', allUsers);
 //get single users
-router.get('/user/:userId', getUser);
+router.get('/user/:userId', requireSignin, getUser);
 
 // any route containing: userId , my app will first Excecute userByid()
 router.param('userId', userById);
