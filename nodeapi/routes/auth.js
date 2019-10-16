1cc9b134ec8f797b4439ddb32f57a9c6c59aef9b
bongomin <bongomindaniel@gmail.com>
2019-10-16 10:50:57 +0300

    feat(SignOut Method):
    - used the clearCookie method to clear cookies for 't'
    
    - clearing cokkies will remove all the rights of the logged in user and log him or her out of the system

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,11 +1,13 @@
-const { signup, signin } = require('../controllers/auth');
+const { signup, signin, signout } = require('../controllers/auth');
 const express = require('express');
 const router = express.Router();
 const { userSignupValidator } = require('../validator');
 
-
+// signup Router
 router.post('/signup', userSignupValidator, signup);
-
+//signin route
 router.post('/signin', signin);
+//signuot
+router.get('/signout', signout);
 
 module.exports = router;
