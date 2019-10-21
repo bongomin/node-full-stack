841e1b3948f17d782898d70e937b37fb5f465c12
bongomin <bongomindaniel@gmail.com>
2019-10-21 17:57:05 +0300

    feat(updating post): - imported extend lodash prcakage to extend the updated posts
    - created updatePost Function that updates the posts

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,14 +1,17 @@
-const { getPosts, createPost, postsByUser, postById, isPoster, deletePost } = require('../controllers/posts');
+const { getPosts, createPost, postsByUser, postById, isPoster, updatePost, deletePost } = require('../controllers/posts');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 const { createPostValidator } = require('../validator');
+const _ = require('lodash');
 
 router.get('/', getPosts);
 router.get('/posts/by/:userId', requireSignin, postsByUser);
 router.post('/posts/new/:userId', requireSignin, createPost, createPostValidator);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+
 
 // any route containing: userId , my app will first Excecute userByid()
 router.param('userId', userById);
