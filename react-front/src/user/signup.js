ab064a85874424fd755f0a17efc52e333832d09c
bongomin <bongomindaniel@gmail.com>
2019-10-22 18:34:17 +0300

    feat(SignUp Form) Created Signup Form
    - using the boostrap Matreial , the boostrap classes are called using className
    
    - gave the Signup a col-8 in all devices

--- react-front/src/user/signup.js
+++ react-front/src/user/signup.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react"
 
 class Signup extends Component {
+
+   // ceraeting a state
+   constructor() {
+      super()
+      this.state = {
+         name: "",
+         email: "",
+         password: "",
+         error: ""
+
+      }
+   }
    render() {
       return (
-         <div>
-            <h2>Signup </h2>
-         </div>
+         <div className="container">
+            <div className="row mt-5 mb-5">
+               <div className="col-8 m-auto">
+                  <h2 className="mt-5 mt-5">Signup </h2>
+                  <hr></hr>
+               </div>
+            </div>
+            <div className="row">
+               <div className="col-8 m-auto">
+                  <form >
+                     <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input type="text" className="form-control"></input>
+                     </div>
+                     <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input type="email" className="form-control"></input>
+                     </div>
+                     <div className="form-group">
+                        <label className="text-muted">password</label>
+                        <input type="password" className="form-control"></input>
+                     </div>
+                     <button className="btn btn-raised btn-primary">Signup</button>
+
+                  </form>
+               </div>
+            </div>
+
+         </div >
       )
    }
 }
