2bc5cf1224040e8834635d827b00e2a1b0839e90
bongomin <bongomindaniel@gmail.com>
2019-10-23 17:39:39 +0300

    feat(SigninForm):
    - created the signin Form component

--- react-front/src/MainRouter.js
+++ react-front/src/MainRouter.js
@@ -3,6 +3,8 @@ import React from "react"
 import { Route, Switch } from "react-router-dom"
 import Home from "./core/Home"
 import Signup from "./user/signup"
+import Signin from "./user/signin"
+
 
 
 const MainRouter = () => (
@@ -11,6 +13,7 @@ const MainRouter = () => (
 
          <Route exact path="/" component={Home}></Route>
          <Route exact path="/signup" component={Signup}></Route>
+         <Route exact path="/signin" component={Signin}></Route>
 
       </Switch>
    </div>
