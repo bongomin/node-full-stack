d7619137799bf6da2664fa971f657405f46abbed
bongomin <bongomindaniel@gmail.com>
2019-10-24 15:15:16 +0300

    feat(Menu,Tab,Links): created the menu, Links and isActive Functionality
    - imported Link, withRouter from react-router-dom to help in creating links and create is active property
    
    - created isActive methord that checks the path,history of the active link and gives it a certain color
    
    - the isActive method is passed on every Link to check is its active
    
    - isActive method contains the history and path as parameters

--- react-front/src/MainRouter.js
+++ react-front/src/MainRouter.js
@@ -4,13 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/signup";
 import Signin from "./user/signin";
+import Menu from "./core/Menu"
 
 
 
 const MainRouter = () => (
    <div>
+      <Menu />
       <Switch>
-
          <Route exact path="/" component={Home}></Route>
          <Route exact path="/signup" component={Signup}></Route>
          <Route exact path="/signin" component={Signin}></Route>
