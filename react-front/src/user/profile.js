acdbe09a4f82800cff3733e18b4012a082022c1c
bongomin <bongomindaniel@gmail.com>
2019-10-29 14:13:31 +0300

    feat(Profile page): Creating a profile page component
    - created a profile page
    
    - the profile page picks the users Id from the packend

---
+++ react-front/src/user/profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class profile extends Component {
+   render() {
+      return (
+         <div className="container">
+            <h2 className="mt-5 mt-5">Profile </h2>
+         </div>
+      )
+   }
+}
+
+export default profile;
